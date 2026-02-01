d0c37935f449f29bfaad35e939839dc1ebe80301
Dmitry Sotnikov <dsotnikov.ph@gmail.com>
2026-02-01 14:12:40 +0100

    basic libraries

---
+++ BiotSavart-function.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams['figure.dpi'] = 300
+import pandas as pd
+
+#%% Generate initial functions
